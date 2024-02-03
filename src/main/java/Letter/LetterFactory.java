1f11a7c9fd3ae9d3a8f69d9ec3d55c5562c3e529
guillaumestpierre <Guillaumestpierre4212003@gmail.com>
2024-02-03 16:10:00 -0500

    il ne reste plus que le A et le N à faire

--- src/main/java/Letter/LetterFactory.java
+++ src/main/java/Letter/LetterFactory.java
@@ -66,7 +66,23 @@ public final class LetterFactory {
      * @return BaseShape containing the letter E
      */
     public static BaseShape create_E() {
-        return null;
+        double zero = 0;
+        Rectangle left = new Rectangle(maxWidth, maxHeight);
+        Rectangle right = new Rectangle(maxWidth-stripeThickness, maxHeight);
+        right.replaceAll(right.translate(right.getCoords(), new Point2d(stripeThickness, zero)));
+        left.remove(right);
+
+        Rectangle barre1 = new Rectangle(maxWidth, stripeThickness);
+        Rectangle barre2 = barre1.clone();
+        barre2.replaceAll(barre2.translate(barre2.getCoords(), new Point2d(zero, halfMaxHeight-stripeThickness/2)));
+        Rectangle barre3 = barre1.clone();
+        barre3.replaceAll(barre3.translate(barre3.getCoords(), new Point2d(zero, -halfMaxHeight-stripeThickness/2)));
+
+        BaseShape E = new BaseShape(left.cloneCoords());
+        E.add(barre1);
+        E.add(barre2);
+        E.add(barre3);
+        return E;
     }
 
     /** TODO
@@ -74,7 +90,15 @@ public final class LetterFactory {
      * @return BaseShape containing the letter H
      */
     public static BaseShape create_H() {
-        return null;
+        Rectangle rExt = new Rectangle(maxWidth, maxHeight);
+        Rectangle rInt = new Rectangle(maxWidth-stripeThickness, maxHeight);
+        rExt.remove(rInt);
+
+        Rectangle barre = new Rectangle(maxWidth, stripeThickness);
+
+        BaseShape H = new BaseShape(rExt.cloneCoords());
+        H.add(barre);
+        return H;
     }
 
     /** TODO
